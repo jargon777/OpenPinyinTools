4d9a1fc39bde9437ded818d102cc42fd65c0ee28
Matthew Muresan <Matthew Muresan@mainwin7-pc>
2016-01-23 20:56:28 +0000

    usable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 '''
 Created on Nov 26, 2015
 
@@ -18,3 +19,75 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 @author: Matthew Muresan
 '''
+import sqlite3
+import sys
+import codecs
+import os
+import re
+from string import punctuation
+
+def main():
+    dbcon = sqlite3.connect("dict/hanzi.sqlite")
+    dbcur = dbcon.cursor()
+    rootdir = os.path.dirname(os.path.realpath(__file__))
+    parsedir = rootdir + "/parse"
+    writedir = rootdir + "/write"
+    try:
+        for i in os.listdir(parsedir):
+            #SRT PARSE MODE
+            if i.endswith(".srt"):
+                writesrtln = ("00:00:00,500 --> 00:00:10,500", "PinYin Added with OpenPinyinTools")
+                writesrttime = "00:00:00,500 --> 00:00:10,500"
+                writesrttextCN = "拼音"
+                writesrttextPY = "Pinyin Added with OpenPinyinTools"
+                writesrtfile = []
+                with open(parsedir + "/" + i, mode="r", encoding="utf8") as readfile:                         
+                    for line in readfile:
+                        #check first for the number.
+                        if line.rstrip('\n').isdigit():
+                            writesrtfile.append((writesrttime, writesrttextCN, writesrttextPY))
+                            print(writesrttextCN, " → ", writesrttextPY)
+                            writesrttime = False
+                            writesrttextCN = False
+                            writesrttextPY = False
+                            
+                        
+                        #check for timestamp
+                        elif ":" in line:
+                            writesrttime = line.rstrip('\n')
+                        
+                        elif line.rstrip('\n') != "":
+                            writesrttextCN = re.sub('{.*?}', '', line.rstrip('\n')) 
+                            writesrttextPY = ""
+                            for cp in writesrttextCN:
+                                try:
+                                    if cp in punctuation:
+                                        writesrttextPY = writesrttextPY + cp                      
+                                    else:
+                                        rowvalid = False
+                                        for row in dbcur.execute("SELECT * FROM dict WHERE hanzi = '%s'" % cp):
+                                            writesrttextPY = writesrttextPY + " " + re.sub(',.*$', '', re.sub('.*?:', '', row[1]))
+                                            rowvalid = True
+                                        if rowvalid == False:
+                                            writesrttextPY = writesrttextPY + cp    
+                                except:
+                                    pass 
+                                
+                print("Done converting. Writing")            
+                with open(writedir + "/" + i, mode="w", encoding="utf8") as writefile:
+                    varcount = 0
+                    for entry in writesrtfile:
+                        varcount += 1
+                        writefile.write(str(varcount))
+                        writefile.write("\n")
+                        writefile.write(entry[0])
+                        writefile.write("\n")
+                        writefile.write(entry[1])
+                        writefile.write("\n")
+                        writefile.write(entry[2])
+                        writefile.write("\n\n")
+    except:
+        print("An error has occured. You should check which files are being updated in this set \n")
+        raise
+if __name__ == "__main__":
+    main()
